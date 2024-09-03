9091af92c47aa9cbc7f1e95b92f0167ae1937f2e
Jung-in Shin <21cscientist@gmail.com>
2024-09-03 23:20:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.getElementById("start-button").addEventListener("click", startTest);
-
 let currentQuestion = 0;
 const totalQuestions = 7;
 let scores = Array(totalQuestions).fill(0);
@@ -14,17 +13,14 @@ const questions = [
     ["27. 속칭 사이버모욕, 사이버명예훼손, 사이버성희롱, 사이버스토킹, 사이버음란물 유통, 대화명 테러, 인증놀이, 게임부주 강요 등 정보통신기기를 이용하여 괴롭히는 행위를 한 적이 있다.", "28. 특정인에 대해 모욕적 언사나 욕설 등을 인터넷 게시판, 채팅, 카페 등에 올리는 행위(특정인에 대한 저격글이 그 한 형태임)를 한 적이 있다.", 
      "29. 특정인에 대한 허위 글이나 개인의 사생활에 관한 사실을 인터넷, SNS 등을 통해 불특정 다수에 공개하는 행위를 한 적이 있다.", "30. 성적 수치심을 주거나, 위협하는 내용, 조롱하는 글, 그림, 동영상 등을 정보통신망을 통해 유포하는 행위를 한 적이 있다.", "31. 공포심이나 불안감을 유발하는 문자, 음향, 영상 등을 휴대폰 등 정보통신망을 통해 반복적으로 보내는 행위를 한 적이 있다.","32. 헤어진 여자친구에게 불안감을 조성하는 문자를 지속적으로 보내는 행위를 한 적이 있다."]
 ];
-
 function startTest() {
     document.getElementById("start-screen").style.display = "none";
     document.getElementById("question-screen").style.display = "block";
     showQuestion();
 }
-
 function showQuestion() {
     const container = document.getElementById("question-container");
     container.innerHTML = '';
-
     questions[currentQuestion].forEach((question, index) => {
         const questionElem = document.createElement('div');
         questionElem.innerHTML = `
@@ -32,28 +28,22 @@ function showQuestion() {
             <div class="answers">
                 <input type="radio" name="q${index}" id="q${index}a1" value="0">
                 <label for="q${index}a1"><span></span>전혀 그렇지 않다</label>
-
                 <input type="radio" name="q${index}" id="q${index}a2" value="1">
                 <label for="q${index}a2"><span></span>가끔 그렇다</label>
-
                 <input type="radio" name="q${index}" id="q${index}a3" value="2">
                 <label for="q${index}a3"><span></span>자주 그렇다</label>
-
                 <input type="radio" name="q${index}" id="q${index}a4" value="3">
                 <label for="q${index}a4"><span></span>항상 그렇다</label>
             </div>
         `;
         container.appendChild(questionElem);
-
         if (selectedAnswers[currentQuestion][index] !== undefined) {
             document.getElementById(`q${index}a${selectedAnswers[currentQuestion][index] + 1}`).checked = true;
         }
     });
-
     updatePageIndicator();
     updateProgressBar();
 }
-
 function updatePageIndicator() {
     let pageIndicator = document.getElementById("page-indicator");
     if (!pageIndicator) {
@@ -63,11 +53,9 @@ function updatePageIndicator() {
     }
     pageIndicator.textContent = `${currentQuestion + 1} / ${totalQuestions}`;
 }
-
 document.getElementById("next-button").addEventListener("click", () => {
     if (validateAnswers()) {
         saveAnswers();
-
         if (currentQuestion < totalQuestions - 1) {
             currentQuestion++;
             showQuestion();
@@ -78,7 +66,6 @@ document.getElementById("next-button").addEventListener("click", () => {
         alert("모든 문항에 답변을 선택해주세요.");
     }
 });
-
 document.getElementById("prev-button").addEventListener("click", () => {
     if (currentQuestion > 0) {
         saveAnswers();
@@ -86,7 +73,6 @@ document.getElementById("prev-button").addEventListener("click", () => {
         showQuestion();
     }
 });
-
 function validateAnswers() {
     let allAnswered = true;
     document.querySelectorAll(`#question-container input[type="radio"]`).forEach((input) => {
@@ -97,13 +83,11 @@ function validateAnswers() {
     });
     return allAnswered;
 }
-
 function saveAnswers() {
     document.querySelectorAll(`#question-container input[type="radio"]:checked`).forEach((input, index) => {
         selectedAnswers[currentQuestion][index] = parseInt(input.value);
     });
 }
-
 function showResult() {
     calculateScores(); // 점수 집계
     document.getElementById("question-screen").style.display = "none";
@@ -111,18 +95,15 @@ function showResult() {
     drawChart();
     displayHighestScoreLabel(); // 가장 높은 항목의 label을 표시
     displayResultText(); // 결과 텍스트 표시
-
     const restartButton = document.createElement("button");
     restartButton.textContent = "처음으로";
     restartButton.addEventListener("click", () => {
         resetTest(); // 테스트 리셋 함수 호출
     });
-
     document.getElementById("result-screen").appendChild(restartButton);
     document.getElementById("page-indicator").style.display = "none";
     document.getElementById("progress-bar-container").style.display = "none";
 }
-
 function resetTest() {
     currentQuestion = 0;
     scores = Array(totalQuestions).fill(0);
@@ -130,50 +111,52 @@ function resetTest() {
     
     document.getElementById("result-screen").style.display = "none";
     document.getElementById("start-screen").style.display = "block";
-    
     // 페이지 인디케이터 다시 설정
     document.getElementById("page-indicator").style.display = "block";
     updatePageIndicator(); // 첫 번째 질문에 맞게 페이지 인디케이터 업데이트
-
-    // averageScore 초기화
-    averageScore = 0;
-
-    // 결과 텍스트 제거
-    const resultScreen = document.getElementById("result-screen");
-    while (resultScreen.firstChild) {
-        resultScreen.removeChild(resultScreen.firstChild);
-    }
 }
-
 function displayHighestScoreLabel() {
     const highestScoreIndex = scores.indexOf(Math.max(...scores));
     const highestScoreLabel = ['신체폭력', '언어폭력', '금품갈취', '강요 행위', '따돌림', '성폭력', '사이버 폭력'][highestScoreIndex];
+    const averageScore = scores.reduce((acc, val) => acc + val, 0) / scores.length;
 
     if (averageScore >= 1) {
         const resultScreen = document.getElementById("result-screen");
         const labelTextElem = document.createElement("p");
         labelTextElem.textContent = `당신의 학폭 MBTI 유형은 '${highestScoreLabel}'형 입니다.`;
+
+    
+          
+            
+    
+
+          
+          Expand Down
+          
+            
+    
+
+          
+          Expand Up
+    
+    @@ -220,7 +220,6 @@ function displayResultText() {
+  
         labelTextElem.style.marginTop = "20px"; // 그래프와의 간격을 위해 여백 추가
         labelTextElem.style.fontSize = "1.2em"; // 텍스트 크기 조정
         labelTextElem.style.textAlign = "center"; // 텍스트 중앙 정렬
         labelTextElem.style.color = "#333"; // 텍스트 색상 설정
         labelTextElem.style.fontWeight = "bold"; // Bold체 적용
-
         resultScreen.appendChild(labelTextElem);
     }
 }
-
 function calculateScores() {
     for (let i = 0; i < totalQuestions; i++) {
         let pageScore = selectedAnswers[i].reduce((acc, val) => acc + val, 0);
         scores[i] = pageScore / questions[i].length; // 평균 점수 계산
     }
-    averageScore = scores.reduce((acc, val) => acc + val, 0) / scores.length; // averageScore 계산
 }
-
 function drawChart() {
     const ctx = document.getElementById('result-chart').getContext('2d');
-
     const data = {
         labels: ['신체폭력', '언어폭력', '금품갈취', '강요 행위', '따돌림', '성폭력', '사이버 폭력'],
         datasets: [{
@@ -184,7 +167,6 @@ function drawChart() {
             borderWidth: 1
         }]
     };
-
     new Chart(ctx, {
         type: 'radar',
         data: data,
@@ -208,10 +190,9 @@ function drawChart() {
         }
     });
 }
-
 function displayResultText() {
+    const averageScore = scores.reduce((acc, val) => acc + val, 0) / scores.length;
     let resultText = '';
-
     if (averageScore < 0.3) {
         resultText = '당신이 겪고 있는 모든 감정은 정상입니다. 불편함, 불안, 혹은 화가 날 수 있습니다. 이러한 감정을 인정하고 표현하는 것이 중요합니다.';
     } else if (averageScore >= 0.3 && averageScore < 0.6) {
@@ -228,6 +209,7 @@ function displayResultText() {
         resultText = '매우 심각한 상황입니다. 담당 교사와 상담을 하세요.';
     }
 
+    // 결과 텍스트를 화면에 표시
     const resultScreen = document.getElementById("result-screen");
     const resultTextElem = document.createElement("p");
     resultTextElem.textContent = resultText;
@@ -235,7 +217,5 @@ function displayResultText() {
     resultTextElem.style.fontSize = "1.2em"; // 텍스트 크기 조정
     resultTextElem.style.textAlign = "center"; // 텍스트 중앙 정렬
     resultTextElem.style.color = "#333"; // 텍스트 색상 설정
-
     resultScreen.appendChild(resultTextElem);
 }
-
